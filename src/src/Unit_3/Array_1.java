103390f6f5813daf50645e6f95b94d63256672ca
deepakjoshi-1397 <deepakjoshiofficial1397@gmail.com>
2025-09-02 11:36:47 +0530

    02_09_2025

---
+++ src/src/Unit_3/Array_1.java
@@ -0,0 +1,11 @@
+package Unit_3;
+import java.util.Scanner;
+public class Array_1 {
+    public static void main(String[] args) {
+        int a[]=new int[15];
+        Scanner sc = new Scanner(System.in);
+        for(int i=0;i<a.length;i++){
+            a[i]=sc.nextInt();
+        }
+    }
+}
