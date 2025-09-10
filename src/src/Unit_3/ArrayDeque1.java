5842e70126a2378fb092dea6ef2a6391758685bd
deepakjoshi-1397 <deepakjoshiofficial1397@gmail.com>
2025-09-10 14:14:22 +0530

    10_09_2025

---
+++ src/src/Unit_3/ArrayDeque1.java
@@ -0,0 +1,16 @@
+package Unit_3;
+
+import java.util.ArrayDeque;
+
+public class ArrayDeque1 {
+    public static void main(String[] args) {
+        ArrayDeque<String> deq=new ArrayDeque<>();
+        deq.addFirst("0");
+        deq.addLast("2nd");
+        System.out.println(deq);
+        System.out.println(deq.offerFirst("-1"));
+        System.out.println(deq.offerLast("3"));
+        System.out.println(deq);
+
+    }
+}
