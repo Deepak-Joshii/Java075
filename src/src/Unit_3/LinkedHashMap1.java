f82bdc1817f96e68c60cef93dde713632fa61444
deepakjoshi-1397 <deepakjoshiofficial1397@gmail.com>
2025-09-16 12:04:05 +0530

    16_09_2025

---
+++ src/src/Unit_3/LinkedHashMap1.java
@@ -0,0 +1,21 @@
+package Unit_3;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
+public class LinkedHashMap1 {
+    public static void main(String[] args) {
+        LinkedHashMap<String,Integer> hm=new LinkedHashMap<>();
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        for(int i=0;i<n;i++){
+            String key=sc.next();
+            int val=sc.nextInt();
+            hm.put(key,val);
+        }
+        for(String k: hm.keySet()){
+            System.out.println(k+" "+hm.get(k));
+        }
+    }
+}
