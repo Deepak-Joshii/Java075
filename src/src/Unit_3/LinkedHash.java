e97ec44f6d0d5e4c83a36ce74f7a4058a2e6cadd
deepakjoshi-1397 <deepakjoshiofficial1397@gmail.com>
2025-09-16 11:05:48 +0530

    12_09_2025

--- src/src/Unit_3/LinkedHash.java
+++ src/src/Unit_3/LinkedHash.java
@@ -1,4 +1,15 @@
 package Unit_3;
 
+import java.util.LinkedHashSet;
+
 public class LinkedHash {
+    public static void main(String[] args) {
+        LinkedHashSet<String>lhs=new LinkedHashSet<>();
+        lhs.add("One");
+        lhs.add("Two");
+        lhs.add("Two");
+        lhs.add("Three");
+        lhs.add("Four");
+        System.out.println(lhs);
+    }
 }
---
+++ src/src/Unit_3/TreeSet1.java
@@ -0,0 +1,15 @@
+package Unit_3;
+
+import java.util.TreeSet;
+
+public class TreeSet1 {
+    public static void main(String[] args) {
+        TreeSet<String> ts=new TreeSet<>();
+        ts.add("1");
+        ts.add("4");
+        ts.add("One");
+        ts.add("0");
+        ts.add("2");
+        System.out.println(ts);
+    }
+}
